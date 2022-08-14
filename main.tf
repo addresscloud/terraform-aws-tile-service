363560309995e445dc6386820bcf5d4385b2a1d4
Tomas Holderness <tomas@addresscloud.com>
2022-08-14 20:44:47 +0100

    add custom bucket policy

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "aws_s3_bucket" "tile" {
 
 resource "aws_s3_bucket_policy" "tile" {
   bucket = aws_s3_bucket.tile.id
-  policy = data.aws_iam_policy_document.bucket.json
+  policy = var.s3_bucket_custom_policy != "" ? var.s3_bucket_custom_policy : data.aws_iam_policy_document.bucket.json
 }
 
 resource "aws_s3_bucket_acl" "tile" {
