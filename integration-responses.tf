ece74d98e1d3e4e905fb7fdbbac5fd919eb61b4f
Tomas Holderness <tomas@addresscloud.com>
2022-08-15 12:39:51 +0100

    add 403 and 404 responses

--- integration-responses.tf
+++ integration-responses.tf
@@ -12,6 +12,20 @@ resource "aws_api_gateway_integration_response" "json_get" {
   }
 }
 
+resource "aws_api_gateway_integration_response" "json_get_404" {
+  rest_api_id = aws_api_gateway_rest_api.tile.id
+  resource_id = aws_api_gateway_resource.tileset.id
+  http_method = aws_api_gateway_method.json_get.http_method
+  status_code = 404
+  depends_on  = [aws_api_gateway_method.json_get, aws_api_gateway_integration.json_get]
+  response_templates = {
+    "application/json" = "{\"message\":\"Not found\"}"
+  }
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = var.api_access_control_allow_origin
+  }
+}
+
 resource "aws_api_gateway_integration_response" "json_options" {
   rest_api_id      = aws_api_gateway_rest_api.tile.id
   resource_id      = aws_api_gateway_resource.tileset.id
@@ -43,6 +57,36 @@ resource "aws_api_gateway_integration_response" "tile_get" {
     "method.response.header.Access-Control-Allow-Origin" = var.api_access_control_allow_origin
     "method.response.header.Content-Type"                = "'application/x-protobuf'"
     "method.response.header.Content-Encoding"            = "'gzip'"
+  }
+}
+
+resource "aws_api_gateway_integration_response" "tile_get_403" {
+  rest_api_id = aws_api_gateway_rest_api.tile.id
+  resource_id = aws_api_gateway_resource.y.id
+  http_method = aws_api_gateway_method.tile_get.http_method
+  status_code = aws_api_gateway_method_response.tile_get.status_code
+  depends_on  = [aws_api_gateway_method.tile_get, aws_api_gateway_integration.tile_get]
+  response_templates = {
+    "application/json" = "{\"message\":\"Forbidden\"}"
+  }
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = var.api_access_control_allow_origin
+    "method.response.header.Content-Type"                = "'application/json'"
+  }
+}
+
+resource "aws_api_gateway_integration_response" "tile_get_404" {
+  rest_api_id = aws_api_gateway_rest_api.tile.id
+  resource_id = aws_api_gateway_resource.y.id
+  http_method = aws_api_gateway_method.tile_get.http_method
+  status_code = aws_api_gateway_method_response.tile_get.status_code
+  depends_on  = [aws_api_gateway_method.tile_get, aws_api_gateway_integration.tile_get]
+  response_templates = {
+    "application/json" = "{\"message\":\"Not found\"}"
+  }
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = var.api_access_control_allow_origin
+    "method.response.header.Content-Type"                = "'application/json'"
   }
 }
 
