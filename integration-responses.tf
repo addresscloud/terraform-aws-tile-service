a1a3e48a38f5f4897039ee7c9439f6a85b796775
Tomas Holderness <tomas@addresscloud.com>
2022-09-07 09:37:09 +0100

    update integration responses

--- integration-responses.tf
+++ integration-responses.tf
@@ -19,6 +19,7 @@ resource "aws_api_gateway_integration_response" "json_get_404" {
   status_code       = 404
   selection_pattern = "404"
   depends_on        = [aws_api_gateway_method.json_get, aws_api_gateway_integration.json_get]
+  content_handling  = "CONVERT_TO_TEXT"
   response_templates = {
     "application/json" = "{\"message\":\"Not found\"}"
   }
@@ -58,22 +59,6 @@ resource "aws_api_gateway_integration_response" "tile_get" {
     "method.response.header.Access-Control-Allow-Origin" = var.api_access_control_allow_origin
     "method.response.header.Content-Type"                = "integration.response.header.Content-Type"
     "method.response.header.Content-Encoding"            = "integration.response.header.Content-Encoding"
-  }
-}
-
-resource "aws_api_gateway_integration_response" "tile_get_403" {
-  rest_api_id       = aws_api_gateway_rest_api.tile.id
-  resource_id       = aws_api_gateway_resource.y.id
-  http_method       = aws_api_gateway_method.tile_get.http_method
-  status_code       = 403
-  selection_pattern = "403"
-  depends_on        = [aws_api_gateway_method.tile_get, aws_api_gateway_integration.tile_get]
-  response_templates = {
-    "application/json" = "{\"message\":\"Forbidden\"}"
-  }
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Origin" = var.api_access_control_allow_origin
-    "method.response.header.Content-Type"                = "'application/json'"
   }
 }
 
