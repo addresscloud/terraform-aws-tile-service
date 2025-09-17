65ab7029a5f09c9e665f89619b53927b90f5830c
Tomas Holderness <tomas@addresscloud.com>
2025-09-17 17:07:08 +0100

    Codex generated updates to error handling

--- integration-responses.tf
+++ integration-responses.tf
@@ -28,6 +28,38 @@ resource "aws_api_gateway_integration_response" "json_get_404" {
   }
 }
 
+resource "aws_api_gateway_integration_response" "json_get_403" {
+  rest_api_id       = aws_api_gateway_rest_api.tile.id
+  resource_id       = aws_api_gateway_resource.tileset.id
+  http_method       = aws_api_gateway_method.json_get.http_method
+  status_code       = aws_api_gateway_method_response.json_get_403.status_code
+  selection_pattern = "403"
+  depends_on        = [aws_api_gateway_method.json_get, aws_api_gateway_integration.json_get, aws_api_gateway_method_response.json_get_403]
+  content_handling  = "CONVERT_TO_TEXT"
+  response_templates = {
+    "application/json" = "{\"message\":\"Request could not be completed\"}"
+  }
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = var.api_access_control_allow_origin
+  }
+}
+
+resource "aws_api_gateway_integration_response" "json_get_500" {
+  rest_api_id       = aws_api_gateway_rest_api.tile.id
+  resource_id       = aws_api_gateway_resource.tileset.id
+  http_method       = aws_api_gateway_method.json_get.http_method
+  status_code       = aws_api_gateway_method_response.json_get_500.status_code
+  selection_pattern = "5\\d{2}"
+  depends_on        = [aws_api_gateway_method.json_get, aws_api_gateway_integration.json_get, aws_api_gateway_method_response.json_get_500]
+  content_handling  = "CONVERT_TO_TEXT"
+  response_templates = {
+    "application/json" = "{\"message\":\"Unexpected error\"}"
+  }
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = var.api_access_control_allow_origin
+  }
+}
+
 resource "aws_api_gateway_integration_response" "json_options" {
   rest_api_id      = aws_api_gateway_rest_api.tile.id
   resource_id      = aws_api_gateway_resource.tileset.id
@@ -72,6 +104,40 @@ resource "aws_api_gateway_integration_response" "tile_get_404" {
   content_handling  = "CONVERT_TO_TEXT"
   response_templates = {
     "application/json" = "{\"message\":\"Not found\"}"
+  }
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = var.api_access_control_allow_origin
+    "method.response.header.Content-Type"                = "'application/json'"
+  }
+}
+
+resource "aws_api_gateway_integration_response" "tile_get_403" {
+  rest_api_id       = aws_api_gateway_rest_api.tile.id
+  resource_id       = aws_api_gateway_resource.y.id
+  http_method       = aws_api_gateway_method.tile_get.http_method
+  status_code       = aws_api_gateway_method_response.tile_get_403.status_code
+  selection_pattern = "403"
+  depends_on        = [aws_api_gateway_method.tile_get, aws_api_gateway_integration.tile_get, aws_api_gateway_method_response.tile_get_403]
+  content_handling  = "CONVERT_TO_TEXT"
+  response_templates = {
+    "application/json" = "{\"message\":\"Request could not be completed\"}"
+  }
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = var.api_access_control_allow_origin
+    "method.response.header.Content-Type"                = "'application/json'"
+  }
+}
+
+resource "aws_api_gateway_integration_response" "tile_get_500" {
+  rest_api_id       = aws_api_gateway_rest_api.tile.id
+  resource_id       = aws_api_gateway_resource.y.id
+  http_method       = aws_api_gateway_method.tile_get.http_method
+  status_code       = aws_api_gateway_method_response.tile_get_500.status_code
+  selection_pattern = "5\\d{2}"
+  depends_on        = [aws_api_gateway_method.tile_get, aws_api_gateway_integration.tile_get, aws_api_gateway_method_response.tile_get_500]
+  content_handling  = "CONVERT_TO_TEXT"
+  response_templates = {
+    "application/json" = "{\"message\":\"Unexpected error\"}"
   }
   response_parameters = {
     "method.response.header.Access-Control-Allow-Origin" = var.api_access_control_allow_origin
@@ -140,6 +206,40 @@ resource "aws_api_gateway_integration_response" "filekey_get_404" {
   }
   response_parameters = {
     "method.response.header.Access-Control-Allow-Origin" = var.api_access_control_allow_origin
+  }
+}
+
+resource "aws_api_gateway_integration_response" "filekey_get_403" {
+  rest_api_id       = aws_api_gateway_rest_api.tile.id
+  resource_id       = aws_api_gateway_resource.filekey.id
+  http_method       = aws_api_gateway_method.filekey_get.http_method
+  status_code       = aws_api_gateway_method_response.filekey_get_403.status_code
+  selection_pattern = "403"
+  depends_on        = [aws_api_gateway_method.filekey_get, aws_api_gateway_integration.filekey_get, aws_api_gateway_method_response.filekey_get_403]
+  content_handling  = "CONVERT_TO_TEXT"
+  response_templates = {
+    "application/json" = "{\"message\":\"Request could not be completed\"}"
+  }
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = var.api_access_control_allow_origin
+    "method.response.header.Content-Type"                = "'application/json'"
+  }
+}
+
+resource "aws_api_gateway_integration_response" "filekey_get_500" {
+  rest_api_id       = aws_api_gateway_rest_api.tile.id
+  resource_id       = aws_api_gateway_resource.filekey.id
+  http_method       = aws_api_gateway_method.filekey_get.http_method
+  status_code       = aws_api_gateway_method_response.filekey_get_500.status_code
+  selection_pattern = "5\\d{2}"
+  depends_on        = [aws_api_gateway_method.filekey_get, aws_api_gateway_integration.filekey_get, aws_api_gateway_method_response.filekey_get_500]
+  content_handling  = "CONVERT_TO_TEXT"
+  response_templates = {
+    "application/json" = "{\"message\":\"Unexpected error\"}"
+  }
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = var.api_access_control_allow_origin
+    "method.response.header.Content-Type"                = "'application/json'"
   }
 }
 
--- method-responses.tf
+++ method-responses.tf
@@ -86,11 +86,79 @@ resource "aws_api_gateway_method_response" "json_get_404" {
   ]
 }
 
+resource "aws_api_gateway_method_response" "json_get_403" {
+  rest_api_id = aws_api_gateway_rest_api.tile.id
+  resource_id = aws_api_gateway_resource.tileset.id
+  http_method = aws_api_gateway_method.json_get.http_method
+  status_code = "403"
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = true
+  }
+  response_models = {
+    "application/json" = "Empty"
+  }
+  depends_on = [
+    aws_api_gateway_method.json_get
+  ]
+}
+
+resource "aws_api_gateway_method_response" "json_get_500" {
+  rest_api_id = aws_api_gateway_rest_api.tile.id
+  resource_id = aws_api_gateway_resource.tileset.id
+  http_method = aws_api_gateway_method.json_get.http_method
+  status_code = "500"
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = true
+  }
+  response_models = {
+    "application/json" = "Empty"
+  }
+  depends_on = [
+    aws_api_gateway_method.json_get
+  ]
+}
+
 resource "aws_api_gateway_method_response" "tile_get_404" {
   rest_api_id = aws_api_gateway_rest_api.tile.id
   resource_id = aws_api_gateway_resource.y.id
   http_method = aws_api_gateway_method.tile_get.http_method
   status_code = "404"
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = true
+    "method.response.header.Content-Encoding"            = true
+    "method.response.header.Content-Type"                = true
+  }
+  response_models = {
+    "application/json" = "Empty"
+  }
+  depends_on = [
+    aws_api_gateway_method.tile_get
+  ]
+}
+
+resource "aws_api_gateway_method_response" "tile_get_403" {
+  rest_api_id = aws_api_gateway_rest_api.tile.id
+  resource_id = aws_api_gateway_resource.y.id
+  http_method = aws_api_gateway_method.tile_get.http_method
+  status_code = "403"
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = true
+    "method.response.header.Content-Encoding"            = true
+    "method.response.header.Content-Type"                = true
+  }
+  response_models = {
+    "application/json" = "Empty"
+  }
+  depends_on = [
+    aws_api_gateway_method.tile_get
+  ]
+}
+
+resource "aws_api_gateway_method_response" "tile_get_500" {
+  rest_api_id = aws_api_gateway_rest_api.tile.id
+  resource_id = aws_api_gateway_resource.y.id
+  http_method = aws_api_gateway_method.tile_get.http_method
+  status_code = "500"
   response_parameters = {
     "method.response.header.Access-Control-Allow-Origin" = true
     "method.response.header.Content-Encoding"            = true
@@ -170,6 +238,46 @@ resource "aws_api_gateway_method_response" "filekey_get_404" {
   resource_id = aws_api_gateway_resource.filekey.id
   http_method = aws_api_gateway_method.filekey_get.http_method
   status_code = "404"
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = true
+    "method.response.header.accept-ranges"               = true
+    "method.response.header.Content-Type"                = true
+    "method.response.header.Content-Encoding"            = true
+    "method.response.header.Content-Length"              = true
+  }
+  response_models = {
+    "application/json" = "Empty"
+  }
+  depends_on = [
+    aws_api_gateway_method.filekey_get
+  ]
+}
+
+resource "aws_api_gateway_method_response" "filekey_get_403" {
+  rest_api_id = aws_api_gateway_rest_api.tile.id
+  resource_id = aws_api_gateway_resource.filekey.id
+  http_method = aws_api_gateway_method.filekey_get.http_method
+  status_code = "403"
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = true
+    "method.response.header.accept-ranges"               = true
+    "method.response.header.Content-Type"                = true
+    "method.response.header.Content-Encoding"            = true
+    "method.response.header.Content-Length"              = true
+  }
+  response_models = {
+    "application/json" = "Empty"
+  }
+  depends_on = [
+    aws_api_gateway_method.filekey_get
+  ]
+}
+
+resource "aws_api_gateway_method_response" "filekey_get_500" {
+  rest_api_id = aws_api_gateway_rest_api.tile.id
+  resource_id = aws_api_gateway_resource.filekey.id
+  http_method = aws_api_gateway_method.filekey_get.http_method
+  status_code = "500"
   response_parameters = {
     "method.response.header.Access-Control-Allow-Origin" = true
     "method.response.header.accept-ranges"               = true
